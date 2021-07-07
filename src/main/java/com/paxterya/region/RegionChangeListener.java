05ffdc048ee886dd7319e235826f199a378e3353
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2021-07-07 21:38:28 +0300

    up version

--- src/main/java/com/paxterya/region/RegionChangeListener.java
+++ src/main/java/com/paxterya/region/RegionChangeListener.java
@@ -9,22 +9,25 @@ import org.bukkit.event.Listener;
 
 public class RegionChangeListener implements Listener {
 
+    private static final TextColor msgColor = TextColor.fromCSSHexString("#919191");
+    private static final String defaultNameColor = "ffffff";
+
     @EventHandler
     public void onRegionChange(RegionChangeEvent event) {
         Player player = event.getPlayer();
         if (event.getNewRegion() == null) {
             player.sendMessage(Component.text()
-                    .append(Component.text("Leaving ").decorate(TextDecoration.ITALIC))
+                    .append(Component.text("Leaving ").color(msgColor).decorate(TextDecoration.ITALIC))
                     .append(Component.text()
                             .content(event.getPreviousRegion().getName())
-                            .color(TextColor.fromCSSHexString("#" + event.getPreviousRegion().getOrDefault("color", "ffffff")))
+                            .color(TextColor.fromCSSHexString("#" + event.getPreviousRegion().getOrDefault("color", defaultNameColor)))
                             .decorate(TextDecoration.ITALIC)));
         } else {
             player.sendMessage(Component.text()
-                    .append(Component.text("Entering ").decorate(TextDecoration.ITALIC))
+                    .append(Component.text("Entering ").color(msgColor).decorate(TextDecoration.ITALIC))
                     .append(Component.text()
                             .content(event.getNewRegion().getName())
-                            .color(TextColor.fromCSSHexString("#" + event.getNewRegion().getOrDefault("color", "ffffff")))
+                            .color(TextColor.fromCSSHexString("#" + event.getNewRegion().getOrDefault("color", defaultNameColor)))
                             .decorate(TextDecoration.ITALIC)));
         }
     }
